386ef351dee38d4c7effad956275300c14006e25
Gulshirin Berdiyeva <gulshirinberdiyeva01@gmail.com>
2022-03-08 19:26:12 +0300

    ADD: mudules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './modules/user.module';
+import { PostModule } from './modules/post.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/prod-prom-statistics'),
+
+    UserModule,
+    PostModule,
+  ],
 })
 export class AppModule {}
